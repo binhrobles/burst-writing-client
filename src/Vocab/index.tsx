62fef76879c4a28c5418eac64f36225ddb859421
Binh 🌱 <binhrobles@gmail.com>
2020-06-05 22:34:14 -0700

    vocab: dynamically allocate word inputs from parent component

--- src/Vocab/index.tsx
+++ src/Vocab/index.tsx
@@ -3,18 +3,38 @@ import { Button, Form, Header } from 'semantic-ui-react';
 import TranslationRequest from './translationRequest';
 
 function Vocab() {
+  const [wordBank, setWordBank] = React.useState(['']);
+
+  function growWordBank() {
+    setWordBank((prev) => prev.concat(''));
+  }
+
+  function getUpdateFunctionForWordAt(idx: number) {
+    return (newWord: string) => {
+      setWordBank((prev) => {
+        const newBank = [...prev];
+        newBank[idx] = newWord;
+        return newBank;
+      });
+    };
+  }
+
   return (
     <>
       <Header as="h3">What words do you need?</Header>
-      <Form>
-        <Form.Group widths="equal">
-          <TranslationRequest />
-        </Form.Group>
+      <Form widths="equal">
+        {wordBank.map((word, idx) => (
+          <TranslationRequest
+            word={word}
+            setWord={getUpdateFunctionForWordAt(idx)}
+            key={idx}
+          />
+        ))}
       </Form>
       <Button
         attached="bottom"
         content="I need another"
-        onClick={() => console.log('add another')}
+        onClick={growWordBank}
       />
     </>
   );
--- src/Vocab/translationRequest.tsx
+++ src/Vocab/translationRequest.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Form } from 'semantic-ui-react';
 import TranslateClient from '../Http/translate';
 
-function TranslationRequest() {
-  const [request, setRequest] = React.useState('');
+function TranslationRequest(props: any) {
+  const { word, setWord } = props;
   const [translation, setTranslation] = React.useState('');
 
   // translates text -> target language
-  // uses `isMounted` to keep track of whether the `request` has changed,
+  // uses `isMounted` to keep track of whether the `word` has changed,
   // and thus the component has unmounted, since the translation response arrived
   React.useEffect(() => {
     let isMounted = true;
     (async () => {
-      const response = await TranslateClient.translate(request, 'es');
+      const response = await TranslateClient.translate(word, 'es'); // TODO: grab lang from user settings
       if (isMounted) setTranslation(response);
     })();
     return () => {
       isMounted = false;
     };
-  }, [request]);
+  }, [word]);
 
   return (
-    <>
+    <Form.Group>
       <Form.Input
         fluid
         placeholder="?"
-        value={request}
-        onChange={(e) => setRequest((e.target as HTMLInputElement).value)}
+        value={word}
+        onChange={(e) => setWord((e.target as HTMLInputElement).value)}
       />
       <Form.Input fluid readOnly value={translation} />
-    </>
+    </Form.Group>
   );
 }
+
+TranslationRequest.propTypes = {
+  word: PropTypes.string.isRequired,
+  setWord: PropTypes.func.isRequired,
+};
 
 export default TranslationRequest;
